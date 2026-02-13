24075e159f2d78987ca619081ec95acde3e7c947
Neracu <kyki89281@gmail.com>
2026-02-14 00:50:15 +0600

    3-rd commit

--- mini_shop/mini_shop/settings.py
+++ mini_shop/mini_shop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'goods.apps.GoodsConfig',
     'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
