92356affa5df6eb1d01d265d8a22746265040b32
Neracu <kyki89281@gmail.com>
2026-02-16 23:57:11 +0600

    Add pagination

--- mini_shop/goods/views.py
+++ mini_shop/goods/views.py
@@ -1,19 +1,35 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug, page=1):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
+
 
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
 
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, 'goods/product.html', context=context)
