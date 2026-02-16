7e96b18dd40397b9547419e50d710f7ca07e9732
Neracu <kyki89281@gmail.com>
2026-02-16 23:02:34 +0600

    Simple tag for categories

--- mini_shop/goods/models.py
+++ mini_shop/goods/models.py
@@ -35,3 +35,9 @@ class Products(models.Model):
 
     def display_id(self):
         return f"{self.id:05}"
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - (self.price * self.discount / 100), 2)
+
+        return self.price
