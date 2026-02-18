f100e16b2cd05cf19b194465acdc59087336e656
Neracu <kyki89281@gmail.com>
2026-02-19 01:05:08 +0600

    Full search done

--- mini_shop/goods/views.py
+++ mini_shop/goods/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
 from goods.utils import q_search
