d7b631ecf1cad0b06a91cf2b074a2e499d21548d
Neracu <kyki89281@gmail.com>
2026-02-19 23:21:52 +0600

    Profile form and HTML

--- mini_shop/users/forms.py
+++ mini_shop/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -12,19 +12,6 @@ class UserLoginForm(AuthenticationForm):
 
     username = forms.CharField()
     password = forms.CharField()
-
-    # username = forms.CharField(
-    #     label="Имя",
-    #     widget=forms.TextInput(attrs={'autofocus': True,
-    #                                   'class': 'form-control',
-    #                                   'placeholder': 'Введите ваше имя пользователя'})
-    # )
-    # password = forms.CharField(
-    #     label="Пароль",
-    #     widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-    #                                       'class': 'form-control',
-    #                                       'placeholder': 'Введите ваш пароль'})
-    # )
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -47,56 +34,21 @@ class UserRegistrationForm(UserCreationForm):
     password1 = forms.CharField()
     password2 = forms.CharField()
 
-    # first_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'Введите ваше имя'
-    #         }
-    #     )
-    # )
-    #
-    # last_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'Введите вашу фамилию'
-    #         }
-    #     )
-    # )
-    #
-    # username = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'Введите ваше имя пользователя'
-    #         }
-    #     )
-    # )
-    #
-    # email = forms.CharField(
-    #     widget=forms.EmailInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'Введите ваше email *youremail@example.com'
-    #         }
-    #     )
-    # )
-    #
-    # password1 = forms.CharField(
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'Введите ваш пароль'
-    #         }
-    #     )
-    # )
-    #
-    # password2 = forms.CharField(
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': 'Подтвердите ваш пароль'
-    #         }
-    #     )
-    # )
+
+class ProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = [
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        ]
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
