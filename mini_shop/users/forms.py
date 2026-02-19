1b39ff702aece51f4c5638fd44ae89f84562c70c
Neracu <kyki89281@gmail.com>
2026-02-19 22:47:58 +0600

    Registration form and HTML

--- mini_shop/users/forms.py
+++ mini_shop/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -27,3 +27,76 @@ class UserLoginForm(AuthenticationForm):
     # )
 
 
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        ]
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #             'placeholder': 'Введите ваше имя'
+    #         }
+    #     )
+    # )
+    #
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #             'placeholder': 'Введите вашу фамилию'
+    #         }
+    #     )
+    # )
+    #
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #             'placeholder': 'Введите ваше имя пользователя'
+    #         }
+    #     )
+    # )
+    #
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #             'placeholder': 'Введите ваше email *youremail@example.com'
+    #         }
+    #     )
+    # )
+    #
+    # password1 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #             'placeholder': 'Введите ваш пароль'
+    #         }
+    #     )
+    # )
+    #
+    # password2 = forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #             'placeholder': 'Подтвердите ваш пароль'
+    #         }
+    #     )
+    # )
