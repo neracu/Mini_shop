f6de0e1307ae67e6561c18da16457fee4ceca416
Neracu <kyki89281@gmail.com>
2026-02-24 21:29:22 +0600

    Admin panel setup

--- mini_shop/goods/admin.py
+++ mini_shop/goods/admin.py
@@ -8,8 +8,22 @@ from goods.models import Category, Products
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name']
 
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'quantity', 'price', 'discount']
+    list_editable = ['discount',]
+    search_fields = ['name', 'description']
+    list_filter = ['discount', 'quantity', 'category']
+    fields = [
+        'name',
+        'category',
+        'slug',
+        'description',
+        'image',
+        ('price', 'discount'),
+        'quantity',
+    ]
