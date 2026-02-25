06cfbf0f9e1b862fa279a794a37277f5aa0b6dcb
Neracu <kyki89281@gmail.com>
2026-02-26 00:03:18 +0600

    validators

--- mini_shop/orders/forms.py
+++ mini_shop/orders/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 
@@ -17,6 +19,21 @@ class CreateOrderForm(forms.Form):
             ('0', False),
             ('1', True),
         ],)
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
+
+
+
 
     # first_name = forms.CharField(
     #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваше имя'}),
